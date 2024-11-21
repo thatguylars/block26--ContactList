14b3fc1b18049a420d782f537ceeb4165606f0b5
thatguylars <ldlb9255@gmail.com>
2024-11-21 13:37:32 -0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+    <div>
+      {selectedContactId ? (
+        <SelectedContact contactId={selectedContactId} /> 
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </div>
+  );}
